28ce8b22b02eba1b4ccb834382bf064823d5620c
Timo Vilen <timo.a.vilen@tuni.fi>
2022-11-29 11:26:13 +0200

    Generic linked list - partially tested

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 char *ordinal(int v)
 {
 	/*
-	   - create this function
-	   - this code won't run with out it!
+		 - create this function
+		 - this code won't run with out it!
 	 */
+
+	
 }
 
 int main()
@@ -13,16 +16,15 @@ int main()
 	int c;
 
 	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	return (0);
 }
